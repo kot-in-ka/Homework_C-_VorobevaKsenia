5ae71ecf902a084bfa0061a9c6668759d2e2c423
Ksenia <vorobeva.ksenia93@mail.ru>
2022-10-01 17:30:20 +0300

    29,27,25

--- Task_29/Program.cs
+++ Task_29/Program.cs
@@ -2,18 +2,50 @@
 // 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
 // 6, 1, 33 -> [6, 1, 33]
 
+// Console.Clear();
+// int lenArray = ReadInt("Введите длинну массива: ");
+
+// int[] randomArray = new int[lenArray];
+// for (int i = 0; i < randomArray.Length; i++)
+// {
+//     randomArray[i] = new Random().Next(1,9);
+//     Console.Write(randomArray[i] + " ");
+// }
+
+// int ReadInt(string message)
+// {
+//     Console.Write(message);
+//     return Convert.ToInt32(Console.ReadLine());
+// }
+
 Console.Clear();
-int lenArray = ReadInt("Введите длинну массива: ");
-
-int[] randomArray = new int[lenArray];
-for (int i = 0; i < randomArray.Length; i++)
+Random random = new Random();
+int size = 8;
+int[] arr = new int[size];
+FillArray(arr);
+Console.WriteLine("Массив: ");
+PrintArray(arr);
+for (int i = 0; i < arr.Length; i++)
+for (int j = i + 1; j < arr.Length; j++)
+if (arr[i] > arr[j])
 {
-    randomArray[i] = new Random().Next(1,9);
-    Console.Write(randomArray[i] + " ");
+    int temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
 }
-
-int ReadInt(string message)
+void FillArray(int[] num)
 {
-    Console.Write(message);
-    return Convert.ToInt32(Console.ReadLine());
+    Random random = new Random();
+    for (int i = 0; i < num.Length; i++)
+    {
+        num[i] = random.Next(-100, 100);
+    }
+}
+void PrintArray(int[] num)
+{
+    for (int i = 0; i < num.Length; i++)
+    {
+        Console.Write(num[i] + " ");
+    }
+    Console.WriteLine();
 }

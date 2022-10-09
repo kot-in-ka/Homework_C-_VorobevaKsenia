1325810c01d17e3caa260d8ef8d1b98e229a80a8
Ksenia <vorobeva.ksenia93@mail.ru>
2022-10-09 13:52:05 +0300

    41, 43

---
+++ Task_41/Program.cs
@@ -0,0 +1,9 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 4
+
+Console.Clear();
+Console.WriteLine("Введите цифры через пробел: ");
+int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+int count = arr.Count(x => x > 0);
+Console.WriteLine($"Колличество чисел больше 0: {count}");
---
+++ Task_43/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых,
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+Console.Clear();
+
+double ReadDouble(string message)
+{
+    Console.Write(message);
+    return Convert.ToDouble(Console.ReadLine());
+}
+double b1 = ReadDouble("Введите b1: ");
+double k1 = ReadDouble("Введите k1: ");
+double b2 = ReadDouble("Введите b2: ");
+double k2 = ReadDouble("Введите k2: ");
+
+double x = -(b1 - b2) / (k1 - k2);
+double y = k1 * x + b1;
+ 
+x = Math.Round(x, 3);
+y = Math.Round(y, 3);
+ 
+Console.WriteLine($"Пересечение двух прямых в точке: ({x};{y})");
